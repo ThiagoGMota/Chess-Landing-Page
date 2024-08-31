a1ff92def37115cbf99cc36265cb51cfcb9354c4
Thiago <thiagogabrielmta@outlook.com.br>
2024-08-31 16:11:28 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}", "./public/*.html", "./assets/**/*.{png,jpg,jpeg,gif,svg}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+
